f516e6034bc18a1e89c86aaa29a9acc8e104c990
onalennaS <onalennahamese07@gmail.com>
2024-11-29 11:54:43 +0200

    i have added the buyer dashboard

--- seller/urls.py
+++ seller/urls.py
@@ -5,5 +5,16 @@ from . import views
 urlpatterns = [
     path('', views.base, name='base'),
     
-  
+   path('user_profile/', views.user_profile, name='user_profile'),
+    
+    
+    path('business_info/', views.business_info, name='business_info'),
+    path('view_stats/', views.view_stats, name='view_stats'),
+    path('add_products/', views.add_products, name='add_products'),
+    path('edit_products/', views.edit_products, name='edit_products'),
+    path('orders/', views.orders, name='orders'),
+    path('order_tracking/', views.order_tracking, name='order_tracking'),
+    path('transaction/', views.transaction, name='transaction'),
+    path('pay_for_premium/', views.pay_for_premium, name='pay_for_premium'),
+
 ]
